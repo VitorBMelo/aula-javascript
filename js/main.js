2f38b1d2dd9424e4002dd9789726f0869241a37b
VitorBMelo <vitorbmelo@gmail.com>
2021-11-01 20:51:04 -0300

    Adicionados exemplos da aula 1

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var d = new Date();
+alert(d);
+
+/*
+var count;
+
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual é a sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}
+*/
+
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas[1].nome);
+*/
+
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor)
+*/
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+
+/*
+var nome = "Vitor Melo";
+var idade = 20;
+var idade2 = 10;
+var frase = "Corinthians é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+*/
