c3cd1718f08e9b764ca3cac7cabd2691bb9ca736
VitorBMelo <vitorbmelo@gmail.com>
2021-11-01 22:45:55 -0300

    Adicionados exemplos da aula 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML  = "Obrigado por clicar";
+}
+
+function redirecionar(){
+    window.open("https://dio.me");
+}
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?")
+console.log(validaIdade(idade))
+*/
+
+/*
 var d = new Date();
 alert(d);
+*/
+
 
 /*
 var count;
